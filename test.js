613c262e35bd2dce4f98e7f6a1a0f609f15f1492
Arun D C <arundc8@gmail.com>
2026-09-14 15:55:52 +1200

    Add test for add function in test.js

---
+++ test.js
@@ -0,0 +1,11 @@
+const add = require("./calculator");
+
+const result = add(2, 3);
+
+if (result === 5) {
+    console.log("Test passed: 2 + 3 equals 5");
+    process.exit(0);
+} else {
+    console.error("Test failed: Expected 5 but received " + result);
+    process.exit(1);
+}
