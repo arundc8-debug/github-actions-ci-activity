bb1996c9a2458e9cbf88b2db3b8cc2df3c66cb1c
Arun D C <arundc8@gmail.com>
2026-09-14 16:04:53 +1200

    Fix test assertion for add function

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const add = require("./calculator");
 
 const result = add(2, 3);
 
-if (result === 6) {
+if (result === 5) {
     console.log("Test passed: 2 + 3 equals 5");
     process.exit(0);
 } else {
